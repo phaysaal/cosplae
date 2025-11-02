32abf1ab71758af3522139398a4f7bff9a4101db
Mahmudul Faisal Al Ameen <mahmudul.faisalalameen@cea.fr>
2025-11-02 23:46:30 +0100

    appending a string inside generated binary

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ use lexer::Lexer;
 use parser::Parser;
 use codegen::Codegen;
 
-mod samplegen;
+// mod samplegen;
+mod elfgen;
+use elfgen::ELFBuilder;
 
 fn main() -> Result<(), std::io::Error> {
     // Example source (fits your Step 6 features)
     
-    /*let source = r#"
+    /* let source = r#"
         struct Point {
             i32 x;
             i32 y;
@@ -38,10 +40,29 @@ fn main() -> Result<(), std::io::Error> {
         Err(e) => {
             println!("❌ {e}");
         }
-    }
-    */
+    } */
+    /* 
     samplegen::emit_min_elf_hello("hello")?;
-    println!("✅ ELF file generated");
+    println!("✅ ELF file generated"); */
+    
+
+    let mut elf = ELFBuilder::new();
+
+    // Minimal "write(1, msg, len); exit(0)" machine code
+    let code: [u8; 42] = [
+        0x48,0xC7,0xC0,0x01,0x00,0x00,0x00,   // mov rax,1
+        0x48,0xC7,0xC7,0x01,0x00,0x00,0x00,   // mov rdi,1
+        0x48,0x8D,0x35,0x0E,0x00,0x00,0x00,   // lea rsi,[rip+0xe]
+        0x48,0xC7,0xC2,0x06,0x00,0x00,0x00,   // mov rdx,6
+        0x0F,0x05,                             // syscall
+        0x48,0xC7,0xC0,0x3C,0x00,0x00,0x00,   // mov rax,60
+        0x48,0x31,0xFF,                       // xor rdi,rdi
+        0x0F,0x05,                             // syscall
+    ];
+
+    elf.append_code(&code);
+    elf.append_str("Hello\n");
+    elf.emit("hello")?;
     Ok(())
 
 }
